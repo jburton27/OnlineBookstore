fe793394c25781c67af23086136d0da53723462b
jburton27 <Joshburton050@gmail.com>
2026-05-03 21:11:17 -0500

    Added missing image and sql file to add images and txt file for queries

---
+++ OnlineBookstore/Data/BookUpdateImagePath.sql
@@ -0,0 +1,304 @@
+USE OnlineBookstore;
+
+UPDATE Book SET ImagePath = '/images/books/The Shining.jpg'
+WHERE BookID = 1;
+
+UPDATE Book SET Title = 'Harry Potter and the Philosopher''s Stone',
+ImagePath = '/images/books/Harry Potter and the Philosopher''s Stone.jpg'
+WHERE BookID = 2;
+
+UPDATE Book SET ImagePath = '/images/books/1984.jpg'
+WHERE BookID = 3;
+
+UPDATE Book SET ImagePath = '/images/books/The Great Gatsby.jpg'
+WHERE BookID = 4;
+
+UPDATE Book SET ImagePath = '/images/books/To Kill a Mockingbird.jpg'
+WHERE BookID = 5;
+
+UPDATE Book SET ImagePath = '/images/books/The Hobbit.jpg'
+WHERE BookID = 6;
+
+UPDATE Book SET ImagePath = '/images/books/The Da Vinci Code.jpg'
+WHERE BookID = 7;
+
+UPDATE Book SET ImagePath = '/images/books/The Hunger Games.jpg'
+WHERE BookID = 8;
+
+UPDATE Book SET ImagePath = '/images/books/And Then There Were None.jpg'
+WHERE BookID = 9;
+
+UPDATE Book SET ImagePath = '/images/books/The Old Man and the Sea.jpg'
+WHERE BookID = 10;
+
+UPDATE Book SET ImagePath = '/images/books/Adventures of Huckleberry Finn.jpg'
+WHERE BookID = 11;
+
+UPDATE Book SET ImagePath = '/images/books/Pride and Prejudice.jpg'
+WHERE BookID = 12;
+
+UPDATE Book SET ImagePath = '/images/books/War and Peace.jpg'
+WHERE BookID = 13;
+
+UPDATE Book SET ImagePath = '/images/books/Frankenstein.jpg'
+WHERE BookID = 14;
+
+UPDATE Book SET ImagePath = '/images/books/Moby-Dick.jpg'
+WHERE BookID = 15;
+
+UPDATE Book SET ImagePath = '/images/books/Great Expectations.jpg'
+WHERE BookID = 16;
+
+UPDATE Book SET ImagePath = '/images/books/The Lion the Witch and the Wardrobe.jpg'
+WHERE BookID = 17;
+
+UPDATE Book SET ImagePath = '/images/books/The Hound of the Baskervilles.jpg'
+WHERE BookID = 18;
+
+UPDATE Book SET ImagePath = '/images/books/The Catcher in the Rye.jpg'
+WHERE BookID = 19;
+
+UPDATE Book SET ImagePath = '/images/books/Slaughterhouse-Five.jpg'
+WHERE BookID = 20;
+
+UPDATE Book SET ImagePath = '/images/books/Fahrenheit 451.jpg'
+WHERE BookID = 21;
+
+UPDATE Book SET ImagePath = '/images/books/Brave New World.jpg'
+WHERE BookID = 22;
+
+UPDATE Book SET ImagePath = '/images/books/Wuthering Heights.jpg'
+WHERE BookID = 23;
+
+UPDATE Book SET ImagePath = '/images/books/Jane Eyre.jpg'
+WHERE BookID = 24;
+
+UPDATE Book SET ImagePath = '/images/books/Dracula.jpg'
+WHERE BookID = 25;
+
+UPDATE Book SET ImagePath = '/images/books/The Picture of Dorian Gray.jpg'
+WHERE BookID = 26;
+
+UPDATE Book SET ImagePath = '/images/books/Of Mice and Men.jpg'
+WHERE BookID = 27;
+
+UPDATE Book SET ImagePath = '/images/books/Lord of the Flies.jpg'
+WHERE BookID = 28;
+
+UPDATE Book SET ImagePath = '/images/books/Catch-22.jpg'
+WHERE BookID = 29;
+
+UPDATE Book SET ImagePath = '/images/books/The Handmaid''s Tale.jpg'
+WHERE BookID = 30;
+
+UPDATE Book SET ImagePath = '/images/books/The Giver.jpg'
+WHERE BookID = 31;
+
+UPDATE Book SET ImagePath = '/images/books/Life of Pi.png'
+WHERE BookID = 32;
+
+UPDATE Book SET ImagePath = '/images/books/The Kite Runner.jpg'
+WHERE BookID = 33;
+
+UPDATE Book SET ImagePath = '/images/books/All the Light We Cannot See.jpg'
+WHERE BookID = 34;
+
+UPDATE Book SET ImagePath = '/images/books/The Goldfinch.png'
+WHERE BookID = 35;
+
+UPDATE Book SET ImagePath = '/images/books/Gone Girl.jpg'
+WHERE BookID = 36;
+
+UPDATE Book SET ImagePath = '/images/books/The Girl on the Train.png'
+WHERE BookID = 37;
+
+UPDATE Book SET ImagePath = '/images/books/The Martian.jpg'
+WHERE BookID = 38;
+
+UPDATE Book SET ImagePath = '/images/books/Dune.jpg'
+WHERE BookID = 39;
+
+UPDATE Book SET ImagePath = '/images/books/The Hitchhiker''s Guide to the Galaxy.jpg'
+WHERE BookID = 40;
+
+UPDATE Book SET ImagePath = '/images/books/A Wrinkle in Time.jpg'
+WHERE BookID = 41;
+
+UPDATE Book SET ImagePath = '/images/books/The Lightning Thief.jpg'
+WHERE BookID = 42;
+
+UPDATE Book SET ImagePath = '/images/books/Divergent.jpg'
+WHERE BookID = 43;
+
+UPDATE Book SET ImagePath = '/images/books/Ender''s Game.jpg'
+WHERE BookID = 44;
+
+UPDATE Book SET ImagePath = '/images/books/Foundation.jpg'
+WHERE BookID = 45;
+
+UPDATE Book SET ImagePath = '/images/books/Do Androids Dream of Electric Sheep.png'
+WHERE BookID = 46;
+
+UPDATE Book SET ImagePath = '/images/books/Jurassic Park.jpg'
+WHERE BookID = 47;
+
+UPDATE Book SET ImagePath = '/images/books/2001 A Space Odyssey.jpg'
+WHERE BookID = 48;
+
+UPDATE Book SET ImagePath = '/images/books/American Gods.jpg'
+WHERE BookID = 49;
+
+UPDATE Book SET ImagePath = '/images/books/Good Omens.jpg'
+WHERE BookID = 50;
+
+UPDATE Book SET ImagePath = '/images/books/Never Let Me Go.jpg' 
+WHERE BookID = 51;
+
+UPDATE Book SET ImagePath = '/images/books/Little Fires Everywhere.jpg' 
+WHERE BookID = 52;
+
+UPDATE Book SET ImagePath = '/images/books/Where the Crawdads Sing.jpg' 
+WHERE BookID = 53;
+
+UPDATE Book SET ImagePath = '/images/books/The Song of Achilles.png' 
+WHERE BookID = 54;
+
+UPDATE Book SET ImagePath = '/images/books/The Nightingale.jpg' 
+WHERE BookID = 55;
+
+UPDATE Book SET ImagePath = '/images/books/Educated.png'
+WHERE BookID = 56;
+
+UPDATE Book SET ImagePath = '/images/books/Born a Crime.jpg' 
+WHERE BookID = 57;
+
+UPDATE Book SET ImagePath = '/images/books/Becoming.jpg' 
+WHERE BookID = 58;
+
+UPDATE Book SET ImagePath = '/images/books/Steve Jobs.jpg' 
+WHERE BookID = 59;
+
+UPDATE Book SET ImagePath = '/images/books/Sapiens.png' 
+WHERE BookID = 60;
+
+UPDATE Book SET ImagePath = '/images/books/Outliers.png' 
+WHERE BookID = 61;
+
+UPDATE Book SET ImagePath = '/images/books/Atomic Habits.jpg' 
+WHERE BookID = 62;
+
+UPDATE Book SET ImagePath = '/images/books/Rich Dad Poor Dad.jpg' 
+WHERE BookID = 63;
+
+UPDATE Book SET ImagePath = '/images/books/The 7 Habits of Highly Effective People.jpg' 
+WHERE BookID = 64;
+
+UPDATE Book SET ImagePath = '/images/books/How to Win Friends and Influence People.jpg' 
+WHERE BookID = 65;
+
+UPDATE Book SET ImagePath = '/images/books/The Power of Now.jpg' 
+WHERE BookID = 66;
+
+UPDATE Book SET ImagePath = '/images/books/The Body Keeps the Score.jpg' 
+WHERE BookID = 67;
+
+UPDATE Book SET ImagePath = '/images/books/The Art of War.jpg' 
+WHERE BookID = 68;
+
+UPDATE Book SET ImagePath = '/images/books/The Prince.jpg' 
+WHERE BookID = 69;
+
+UPDATE Book SET ImagePath = '/images/books/The Odyssey.jpg' 
+WHERE BookID = 70;
+
+UPDATE Book SET ImagePath = '/images/books/The Aeneid.jpg' 
+WHERE BookID = 71;
+
+UPDATE Book SET ImagePath = '/images/books/Oedipus Rex.jpg' 
+WHERE BookID = 72;
+
+UPDATE Book SET ImagePath = '/images/books/Inferno.jpg' 
+WHERE BookID = 73;
+
+UPDATE Book SET ImagePath = '/images/books/Don Quixote.jpg' 
+WHERE BookID = 74;
+
+UPDATE Book SET ImagePath = '/images/books/Les Miserables.jpg' 
+WHERE BookID = 75;
+
+UPDATE Book SET ImagePath = '/images/books/The Count of Monte Cristo.jpg' 
+WHERE BookID = 76;
+
+UPDATE Book SET ImagePath = '/images/books/A Journey to the Center of the Earth.jpg' 
+WHERE BookID = 77;
+
+UPDATE Book SET ImagePath = '/images/books/The Time Machine.jpg' 
+WHERE BookID = 78;
+
+UPDATE Book SET ImagePath = '/images/books/The Call of the Wild.jpg' 
+WHERE BookID = 79;
+
+UPDATE Book SET ImagePath = '/images/books/The Age of Innocence.jpg' 
+WHERE BookID = 80;
+
+UPDATE Book SET ImagePath = '/images/books/The Jungle.jpg' 
+WHERE BookID = 81;
+
+UPDATE Book SET ImagePath = '/images/books/My Antonia.jpg' 
+WHERE BookID = 82;
+
+UPDATE Book SET ImagePath = '/images/books/Their Eyes Were Watching God.jpg' 
+WHERE BookID = 83;
+
+UPDATE Book SET ImagePath = '/images/books/Beloved.jpg' 
+WHERE BookID = 84;
+
+UPDATE Book SET ImagePath = '/images/books/The Color Purple.jpg' 
+WHERE BookID = 85;
+
+UPDATE Book SET ImagePath = '/images/books/Things Fall Apart.jpg' 
+WHERE BookID = 86;
+
+UPDATE Book SET ImagePath = '/images/books/One Hundred Years of Solitude.jpg' 
+WHERE BookID = 87;
+
+UPDATE Book SET ImagePath = '/images/books/The House of the Spirits.jpg' 
+WHERE BookID = 88;
+
+UPDATE Book SET ImagePath = '/images/books/Interpreter of Maladies.jpg' 
+WHERE BookID = 89;
+
+UPDATE Book SET ImagePath = '/images/books/The Poisonwood Bible.jpg' 
+WHERE BookID = 90;
+
+UPDATE Book SET ImagePath = '/images/books/Bel Canto.jpg' 
+WHERE BookID = 91;
+
+UPDATE Book SET ImagePath = '/images/books/A Man Called Ove.jpg' 
+WHERE BookID = 92;
+
+UPDATE Book SET ImagePath = '/images/books/The Midnight Library.jpg' 
+WHERE BookID = 93;
+
+UPDATE Book SET ImagePath = '/images/books/Normal People.png' 
+WHERE BookID = 94;
+
+UPDATE Book SET ImagePath = '/images/books/The Seven Husbands of Evelyn Hugo.jpg' 
+WHERE BookID = 95;
+
+UPDATE Book SET ImagePath = '/images/books/The Vanishing Half.jpg' 
+WHERE BookID = 96;
+
+UPDATE Book SET ImagePath = '/images/books/The Underground Railroad.jpg' 
+WHERE BookID = 97;
+
+UPDATE Book SET ImagePath = '/images/books/On Earth We''re Briefly Gorgeous.png' 
+WHERE BookID = 98;
+
+UPDATE Book SET ImagePath = '/images/books/A Gentleman in Moscow.png' 
+WHERE BookID = 99;
+
+UPDATE Book SET ImagePath = '/images/books/Kitchen Confidential.jpg' 
+WHERE BookID = 100;
+
+SELECT * FROM Book;
